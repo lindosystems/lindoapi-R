a259552abcf9b4889fa1b3a50e9b742680e1b6c8
Mustafa Atlihan <mkatlihan@gmail.com>
2024-10-30 00:08:24 +0400

    arm64

--- install-rlindo.sh
+++ install-rlindo.sh
@@ -52,13 +52,17 @@ if [ ${OSYS} = "Darwin" ]; then
 	if [ -f "${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so" ]; then
 		echo changing the install name of rLindo package
 		if [ ${MACHINE} = "x86_64" ]; then
-			install_name_tool -change @loader_path/liblindo64.dylib \
-							${LINDOAPI_HOME}/bin/osx64x86/liblindo64.$LS_MAJOR.$LS_MINOR.dylib \
-							${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so
+		    install_name_tool -change @loader_path/liblindo64.dylib \
+				      ${LINDOAPI_HOME}/bin/osx64x86/liblindo64.$LS_MAJOR.$LS_MINOR.dylib \
+				      ${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so
+		elif [ ${MACHINE} = "arm64" ]; then
+		    install_name_tool -change @loader_path/liblindo64.dylib \
+				      ${LINDOAPI_HOME}/bin/osx64arm/liblindo64.$LS_MAJOR.$LS_MINOR.dylib \
+				      ${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so
 		else
-			install_name_tool -change @loader_path/liblindo.dylib \
-							${LINDOAPI_HOME}/bin/osx32x86/liblindo.$LS_MAJOR.$LS_MINOR.dylib \
-							${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so
+		    install_name_tool -change @loader_path/liblindo.dylib \
+				      ${LINDOAPI_HOME}/bin/osx32x86/liblindo.$LS_MAJOR.$LS_MINOR.dylib \
+				      ${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so
 		fi
 	else
 		echo >&2 "Error: ${R_HOME}/$LIBRARY/rLindo/libs/rLindo.so is not found"
